413f73c5ade757cfb88b611ec47e2b2a8aa2b48c
karshoutman <106554760+karshoutman@users.noreply.github.com>
2023-01-25 00:25:13 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
-console.log("hi");
+console.log("hallo!");
 
-// switch tussen weergave hamburger menu
-function hamburgermenu() {
-    document.getElementById("list").classList.toggle("show");
-  }
+
+// Variabelen
+var menu = document.querySelector(".dropdown-menu");
+var img = document.querySelector(".menu-img");
+
+
+// Functie om te switchen in het menu
+function toggleMenu() {
+  menu.classList.toggle("show");
+}
+
+
+// AddEventListener om de functie aan te roepen
+img.addEventListener("click", toggleMenu);
